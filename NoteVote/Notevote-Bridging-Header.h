f23fd5522e628f83b0dac2bc397aa61c800a3266
NoteVote <bass4aj@gmail.com>
2015-09-15 16:45:36 -0500

    initial commit

---
+++ NoteVote/Notevote-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Notevote-Bridging-Header.h
+//  NoteVote
+//
+//  Created by uics15 on 9/10/15.
+//  Copyright © 2015 NoteVote. All rights reserved.
+//
+
+#ifndef Notevote_Bridging_Header_h
+#define Notevote_Bridging_Header_h
+
+#import <Spotify/Spotify.h>
+
+#endif /* Notevote_Bridging_Header_h */
